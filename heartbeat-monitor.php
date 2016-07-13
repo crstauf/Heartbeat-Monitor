656ff6dd8f7d76e79ae124b6163388bf1d68ac68
Caleb Stauffer <develop@calebstauffer.com>
2016-07-12 23:51:02 -0400

    add security

--- heartbeat-monitor.php
+++ heartbeat-monitor.php
@@ -8,6 +8,12 @@ Author: Caleb Stauffer
 Author URI: http://develop.calebstauffer.com
 */
 
+if (!defined('ABSPATH') || !function_exists('add_filter')) {
+	header( 'Status: 403 Forbidden' );
+	header( 'HTTP/1.1 403 Forbidden' );
+	exit();
+}
+
 if (!defined('DOING_AJAX') || !DOING_AJAX)
 	add_action('init',array('css_heartbeat_monitor','action_init'));
 
