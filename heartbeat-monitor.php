f664f759e61cdb8cdf4c7dd0db199a9c6698e083
Caleb Stauffer <develop@calebstauffer.com>
2016-07-24 01:13:52 -0400

    support for other admin themes

--- heartbeat-monitor.php
+++ heartbeat-monitor.php
@@ -29,9 +29,10 @@ class css_heartbeat_monitor {
 
 		global $wp_current_filter,$wp_scripts;
 		$current_filter = $wp_current_filter[0];
+
 		if (
 			isset($wp_scripts) && isset($wp_scripts->registered) &&
-			is_array($wp_scripts->registered) && count($wp_scripts->registered) && 
+			is_array($wp_scripts->registered) && count($wp_scripts->registered) &&
 			array_key_exists('heartbeat',$wp_scripts->registered) &&
 			false === array_search('heartbeat-monitor-lub',$wp_scripts->registered['heartbeat']->deps)
 		)
@@ -45,14 +46,13 @@ class css_heartbeat_monitor {
 			console.groupEnd();'
 		);
 
-		wp_register_style('heartbeat-monitor',plugin_dir_url(__FILE__) . 'styles.css',array(),'0.0.3');
 		wp_register_script('heartbeat-monitor-lub',plugin_dir_url(__FILE__) . 'lub.js',array('jquery'),'0.0.3');
 		wp_register_script('heartbeat-monitor-dub',plugin_dir_url(__FILE__) . 'dub.js',array('jquery','heartbeat','heartbeat-monitor-lub'),'0.0.3');
 		wp_register_script('heartbeat-monitor-nobeat',plugin_dir_url(__FILE__) . 'nobeat.js',array('jquery'),'0.0.3');
 
 		if (false === wp_script_is('heartbeat')) return;
 
-		wp_enqueue_style('heartbeat-monitor');
+		self::css();
 		self::$alive = true;
 	}
 
@@ -61,8 +61,7 @@ class css_heartbeat_monitor {
 			self::no_heartbeat_detected();
 			return;
 		} else if (false === self::$alive) {
-			global $wp_styles;
-			echo '<link rel="stylesheet" href="' . $wp_styles->registered['heartbeat-monitor']->src . '?ver=' . $wp_styles->registered['heartbeat-monitor']->ver . '" type="text/css" />';
+			self::css();
 			self::$alive = true;
 		}
 
@@ -88,6 +87,82 @@ class css_heartbeat_monitor {
 	public static function dub() {
 		global $wp_scripts;
 		$wp_scripts->print_scripts('heartbeat-monitor-dub');
+	}
+
+	public static function css() {
+		list($scheme) = get_user_meta(get_current_user_id(),'admin_color');
+		$colors = array(
+			'fresh'     => 'rgba(35, 40, 45, 1.0)',
+			'light'     => 'rgba(229, 229, 229, 1.0)',
+			'blue'      => 'rgba(82, 172, 204, 1.0)',
+			'coffee'    => 'rgba(89, 82, 76, 1.0)',
+			'ectoplasm' => 'rgba(82, 63, 109, 1.0)',
+			'midnight'  => 'rgba(54, 59, 63, 1.0)',
+			'ocean'     => 'rgba(115, 142, 150, 1.0)',
+			'sunrise'   => 'rgba(207, 73, 68, 1.0)',
+		);
+		?>
+
+		<style type="text/css">
+
+			#wpadminbar { transition: 0.2s background ease; }
+			#wpadminbar.heartbeat-lub { background-color: #900; }
+
+			@-webkit-keyframes heartbeatirregular {
+				0%   { background-color: #900; }
+				20%  { background-color: <?php echo $colors[$scheme] ?>; }
+				80%  { background-color: <?php echo $colors[$scheme] ?>; }
+				100% { background-color: #900; }
+			}
+			@-moz-keyframes heartbeatirregular {
+				0%   { background-color: #900; }
+				20%  { background-color: <?php echo $colors[$scheme] ?>; }
+				80%  { background-color: <?php echo $colors[$scheme] ?>; }
+				100% { background-color: #900; }
+			}
+			@-ms-keyframes heartbeatirregular {
+				0%   { background-color: #900; }
+				20%  { background-color: <?php echo $colors[$scheme] ?>; }
+				80%  { background-color: <?php echo $colors[$scheme] ?>; }
+				100% { background-color: #900; }
+			}
+
+			#wpadminbar.heartbeat-irregular {
+				-webkit-animation: heartbeatirregular 2s infinite;
+				   -moz-animation: heartbeatirregular 2s infinite;
+				    -ms-animation: heartbeatirregular 2s infinite;
+			}
+
+			@-webkit-keyframes heartbeatshock {
+				0%   { background-color: #FFF; }
+				2%   { background-color: #FFF; }
+				3%   { background-color: <?php echo $colors[$scheme] ?>; }
+				100% { background-color: <?php echo $colors[$scheme] ?>; }
+			}
+			@-moz-keyframes heartbeatshock {
+				0%   { background-color: #FFF; }
+				2%   { background-color: #FFF; }
+				3%   { background-color: <?php echo $colors[$scheme] ?>; }
+				100% { background-color: <?php echo $colors[$scheme] ?>; }
+			}
+			@-ms-keyframes heartbeatshock {
+				0%   { background-color: #FFF; }
+				2%   { background-color: #FFF; }
+				3%   { background-color: <?php echo $colors[$scheme] ?>; }
+				100% { background-color: <?php echo $colors[$scheme] ?>; }
+			}
+
+			#wpadminbar.heartbeat-shock {
+				-webkit-animation: heartbeatshock 12s infinite;
+				   -moz-animation: heartbeatshock 12s infinite;
+				    -ms-animation: heartbeatshock 12s infinite;
+			}
+
+			#wp-toolbar ul.ab-top-menu > li { background-color: <?php echo str_replace('1.0','0.9',$colors[$scheme]) ?>; }
+
+		</style>
+
+		<?php
 	}
 
 }
